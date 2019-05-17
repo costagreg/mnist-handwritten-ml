571c7868950edde90f9ce09411b494a57006b151
Greg <costagregorioalessio@gmail.com>
2019-05-17 10:26:13 +0100

    Playing with api.py

--- api.py
+++ api.py
@@ -1,5 +1,9 @@
 from flask import Flask , request, jsonify
+from flask_cors import CORS
+
 import numpy as np 
+import base64
+import cv2
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
@@ -9,6 +13,28 @@ import tensorflow as tf
 # W1 = tf.train.load_variable(get_checkpoint, 'W1')
 # W2 = tf.train.load_variable(get_checkpoint, 'W2')
 # b1 = tf.train.load_variable(get_checkpoint, 'b1')
+
+def data_uri_to_cv2_img(uri):
+  encoded_data = uri.split(',')[1]
+  filename = 'canvas_image.png'
+  imgdata = base64.b64decode(encoded_data)
+  with open(filename, 'wb') as f:
+    f.write(imgdata)
+  
+  img = cv2.imread('canvas_image.png', 0)
+
+  return img
+
+def ValueInvert(array):
+    # Flatten the array for looping
+    flatarray = array.flatten()
+    
+    # Apply transformation to flattened array
+    for i in range(flatarray.size):
+        flatarray[i] = 1 - flatarray[i]
+        
+    # Return the transformed array, with the original shape
+    return flatarray.reshape(array.shape)
 
 layer_1 = 784
 layer_2 = 20
@@ -32,14 +58,26 @@ sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 imported_graph.restore(sess, './tmp/mini_batch-23000')
 
+
 app = Flask(__name__)
+CORS(app)
 @app.route('/recognize', methods = ['POST'])
 def recognize():
   request_data = request.get_json()
-  img = np.array(request_data['data'])
-  X_1 = img.reshape(img.shape[0], 1)
-  X_1 = X_1/255.
-  print(X_1)
+  imgbase64 = request_data['data']
+  encoded_data = imgbase64.split(',')[1]
+  filename = 'canvas_image.png'
+  imgdata = base64.b64decode(encoded_data)
+  with open(filename, 'wb') as f:
+    f.write(imgdata)
+  
+  img = cv2.imread('canvas_image.png', cv2.IMREAD_GRAYSCALE)
+  small = cv2.resize(img, (28, 28))
+  cv2.imwrite('resize.png', small)
+  data = cv2.imread('resize.png', cv2.IMREAD_GRAYSCALE)
+
+  data = data.reshape(28*28, 1)/255.
+  X_1 = data
   test, pred_test = sess.run([Z2, Y_hat], feed_dict={ X: X_1 })
   print(test)
   print(np.argmax(test, axis=0))
