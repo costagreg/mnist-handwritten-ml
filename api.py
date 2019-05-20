a0f174409960533ad64d935292e5ed316d8a670c
Greg <costagregorioalessio@gmail.com>
2019-05-20 14:40:48 +0100

    Add more training images

--- api.py
+++ api.py
@@ -30,11 +30,11 @@ def data_uri_to_cv2_img(uri):
 # 2 layer  NN
 
 sess = tf.Session()
-saver = tf.train.import_meta_graph('./tmp/2layers_test63-16150.meta')
+saver = tf.train.import_meta_graph('./tmp/2layers_test74-1000.meta')
 saver.restore(sess, tf.train.latest_checkpoint('./tmp/'))
 graph = tf.get_default_graph()
 X = graph.get_tensor_by_name("X:0")
-Y_hat = graph.get_tensor_by_name("Add_2:0")
+Y_hat = graph.get_tensor_by_name("Y_hat:0")
 
 app = Flask(__name__)
 CORS(app)
@@ -77,7 +77,82 @@ def save_dev():
   
   img = cv2.imread('canvas_image.png', cv2.IMREAD_GRAYSCALE)
   small = cv2.resize(img, (28, 28))
-  cv2.imwrite('./dev_images/'+str(number)+'/'+ str(np.random.randint(0, high=100000000000000000)) +'.png', small)
+  # Add padding of 100 px and translate image
+  img_with_padding = cv2.copyMakeBorder(img, 30, 30, 30, 30, cv2.BORDER_CONSTANT, value=[255,255,255])
+  img_with_padding_resized = cv2.resize(img_with_padding, (28,28))
+
+  rows, cols = img_with_padding.shape
+  
+  # Translate images
+  M1 = np.float32([[1,0,30],[0,1,0]])
+  M2 = np.float32([[1,0,0],[0,1,30]])
+  M3 = np.float32([[1,0,-30],[0,1,0]])
+  M4 = np.float32([[1,0,0],[0,1,-30]])
+
+  M1_img = cv2.warpAffine(img_with_padding,M1,(cols,rows), borderMode=cv2.BORDER_CONSTANT, borderValue=[255,255,255])
+  M2_img = cv2.warpAffine(img_with_padding,M2,(cols,rows), borderMode=cv2.BORDER_CONSTANT, borderValue=[255,255,255])
+  M3_img = cv2.warpAffine(img_with_padding,M3,(cols,rows), borderMode=cv2.BORDER_CONSTANT, borderValue=[255,255,255])
+  M4_img = cv2.warpAffine(img_with_padding,M4,(cols,rows), borderMode=cv2.BORDER_CONSTANT, borderValue=[255,255,255])
+
+  M1_img_resized = cv2.resize(M1_img, (28,28))
+  M2_img_resized = cv2.resize(M2_img, (28,28))
+  M3_img_resized = cv2.resize(M3_img, (28,28))
+  M4_img_resized = cv2.resize(M4_img, (28,28))
+  
+  # Rotate image matrix 
+  R_1 = cv2.getRotationMatrix2D((cols/2,rows/2),15,1)
+  R_2 = cv2.getRotationMatrix2D((cols/2,rows/2),-15,1)
+
+
+  M1_R_1_img = cv2.warpAffine(M1_img,R_1,(cols,rows), borderMode=cv2.BORDER_CONSTANT, borderValue=[255,255,255])
+  M1_R_2_img = cv2.warpAffine(M1_img,R_2,(cols,rows), borderMode=cv2.BORDER_CONSTANT, borderValue=[255,255,255])
+
+  M2_R_1_img = cv2.warpAffine(M2_img,R_1,(cols,rows), borderMode=cv2.BORDER_CONSTANT, borderValue=[255,255,255])
+  M2_R_2_img = cv2.warpAffine(M2_img,R_2,(cols,rows), borderMode=cv2.BORDER_CONSTANT, borderValue=[255,255,255])
+
+  M3_R_1_img = cv2.warpAffine(M3_img,R_1,(cols,rows), borderMode=cv2.BORDER_CONSTANT, borderValue=[255,255,255])
+  M3_R_2_img = cv2.warpAffine(M3_img,R_2,(cols,rows), borderMode=cv2.BORDER_CONSTANT, borderValue=[255,255,255])
+
+  M4_R_1_img = cv2.warpAffine(M4_img,R_1,(cols,rows), borderMode=cv2.BORDER_CONSTANT, borderValue=[255,255,255])
+  M4_R_2_img = cv2.warpAffine(M4_img,R_2,(cols,rows), borderMode=cv2.BORDER_CONSTANT, borderValue=[255,255,255])
+
+  M1_R_1_img_resized = cv2.resize(M1_R_1_img, (28,28))
+  M1_R_2_img_resized  = cv2.resize(M1_R_2_img, (28,28))
+  M2_R_1_img_resized  = cv2.resize(M2_R_1_img, (28,28))
+  M2_R_2_img_resized  = cv2.resize(M2_R_2_img, (28,28))
+  M3_R_1_img_resized = cv2.resize(M3_R_1_img, (28,28))
+  M3_R_2_img_resized = cv2.resize(M3_R_2_img, (28,28))
+  M4_R_1_img_resized = cv2.resize(M4_R_1_img, (28,28))
+  M4_R_2_img_resized = cv2.resize(M4_R_2_img, (28,28))
+
+  cv2.imwrite('img_with_padding_resized.png', img_with_padding_resized)
+  cv2.imwrite('M1_img.png', M1_img_resized)
+  cv2.imwrite('M2_img.png', M2_img_resized)
+  cv2.imwrite('M3_img.png', M3_img_resized)
+  cv2.imwrite('M4_img.png', M4_img_resized)
+  cv2.imwrite('M1_R_1_img.png', M1_R_1_img_resized)
+  cv2.imwrite('M1_R_2_img.png', M1_R_2_img_resized)
+  cv2.imwrite('M2_R_1_img.png', M2_R_1_img_resized)
+  cv2.imwrite('M2_R_2_img.png', M2_R_2_img_resized)
+  cv2.imwrite('M3_R_1_img.png', M3_R_1_img_resized)
+  cv2.imwrite('M3_R_2_img.png', M3_R_2_img_resized)
+  cv2.imwrite('M4_R_1_img.png', M4_R_1_img_resized)
+  cv2.imwrite('M4_R_2_img.png', M4_R_2_img_resized)
+
+  cv2.imwrite('./training_images/'+str(number)+'/'+ str(np.random.randint(0, high=100000000000000000)) +'.png', small)
+  cv2.imwrite('./training_images/'+str(number)+'/'+ str(np.random.randint(0, high=100000000000000000)) +'.png', img_with_padding_resized)
+  cv2.imwrite('./training_images/'+str(number)+'/'+ str(np.random.randint(0, high=100000000000000000)) +'.png', M1_img_resized)
+  cv2.imwrite('./training_images/'+str(number)+'/'+ str(np.random.randint(0, high=100000000000000000)) +'.png', M2_img_resized)
+  cv2.imwrite('./training_images/'+str(number)+'/'+ str(np.random.randint(0, high=100000000000000000)) +'.png', M3_img_resized)
+  cv2.imwrite('./training_images/'+str(number)+'/'+ str(np.random.randint(0, high=100000000000000000)) +'.png', M4_img_resized)
+  cv2.imwrite('./training_images/'+str(number)+'/'+ str(np.random.randint(0, high=100000000000000000)) +'.png', M1_R_1_img_resized)
+  cv2.imwrite('./training_images/'+str(number)+'/'+ str(np.random.randint(0, high=100000000000000000)) +'.png', M1_R_2_img_resized)
+  cv2.imwrite('./training_images/'+str(number)+'/'+ str(np.random.randint(0, high=100000000000000000)) +'.png', M2_R_1_img_resized)
+  cv2.imwrite('./training_images/'+str(number)+'/'+ str(np.random.randint(0, high=100000000000000000)) +'.png', M2_R_2_img_resized)
+  cv2.imwrite('./training_images/'+str(number)+'/'+ str(np.random.randint(0, high=100000000000000000)) +'.png', M3_R_1_img_resized)
+  cv2.imwrite('./training_images/'+str(number)+'/'+ str(np.random.randint(0, high=100000000000000000)) +'.png', M3_R_2_img_resized)
+  cv2.imwrite('./training_images/'+str(number)+'/'+ str(np.random.randint(0, high=100000000000000000)) +'.png', M4_R_1_img_resized)
+  cv2.imwrite('./training_images/'+str(number)+'/'+ str(np.random.randint(0, high=100000000000000000)) +'.png', M4_R_2_img_resized)
   
   return jsonify({}), 200
 
