c7c2ad7da73b384874e5b08845739a547125d9dd
Greg <costagregorioalessio@gmail.com>
2019-05-21 16:07:52 +0100

    Implemented center image and playing around

--- api.py
+++ api.py
@@ -6,6 +6,7 @@ import base64
 import cv2
 import matplotlib.pyplot as plt
 import tensorflow as tf
+import os
 from utils import ValueInvert
 
 
@@ -36,8 +37,81 @@ def data_uri_to_cv2_img(uri):
 # X = graph.get_tensor_by_name("X:0")
 # Y_hat = graph.get_tensor_by_name("Y_hat:0")
 
+def find_center_image(img):
+  left = 0
+  right = img.shape[1] - 1
+
+  empty_left = True
+  empty_right = True
+
+  for col in range(int(img.shape[1])):
+    if empty_left == False and empty_right == False:
+      break
+
+    for row in range(img.shape[0] - 1):
+      # print(img[row, col])
+      if img[row, col] < 255 and empty_left == True:
+        empty_left = False
+        left = col
+
+      if img[row, img.shape[1] - col - 1] < 255 and empty_right == True:
+        empty_right = False
+        right = img.shape[1] - col
+
+
+  top = 0
+  bottom = img.shape[0] - 1
+
+  empty_top = True
+  empty_bottom = True
+
+  for row in range(int(img.shape[0])):
+    if empty_top == False and empty_bottom == False:
+      break
+
+    for col in range(img.shape[1] - 1):
+      if img[row, col] < 255 and empty_top == True:
+        empty_top = False
+        top = row
+
+      if img[img.shape[0] - row - 1, col] < 255 and empty_bottom == True:
+        empty_bottom = False
+        bottom = img.shape[0] - row
+
+
+  # print('left ', str(left))
+  # print('top ', str(top))
+  # print('right ', str(right))
+  # print('bottom ', str(bottom))
+    
+  # plt.imshow(img)
+  # plt.show()
+
+  return top, right, bottom, left
+
+def center_image(img):
+  cols, rows = img.shape
+  top, right, bottom, left = find_center_image(img)
+  width = right - left
+  height = bottom - top
+  center_digit_x = (width / 2) + left
+  center_digit_y = (height / 2) + top
+  center_img_x = cols / 2
+  center_img_y = rows / 2
+  t_x = center_img_x - center_digit_x
+  t_y = center_img_y - center_digit_y
+
+  # print(t_x)
+  # print(t_y)
+  M = np.float32([[1,0,t_x],[0,1,t_y]])
+  dst = cv2.warpAffine(img,M,(cols,rows), borderMode=cv2.BORDER_CONSTANT, borderValue=[255,255,255])
+  # plt.imshow(dst)
+  # plt.show()
+  return dst
+
 app = Flask(__name__)
 CORS(app)
+
 @app.route('/recognize', methods = ['POST'])
 def recognize():
   request_data = request.get_json()
@@ -53,6 +127,8 @@ def recognize():
   cv2.imwrite('resize.png', small)
   data = cv2.imread('resize.png', cv2.IMREAD_GRAYSCALE)
   data = ValueInvert(data)/255.
+  # Center image here
+  X_1 = center_image(data)
   # print(data)
   X_1 = data.reshape(28 * 28, 1)
   print(X_1)
@@ -77,84 +153,35 @@ def save_dev():
   
   img = cv2.imread('canvas_image.png', cv2.IMREAD_GRAYSCALE)
   small = cv2.resize(img, (28, 28))
-  # Add padding of 100 px and translate image
-  img_with_padding = cv2.copyMakeBorder(img, 30, 30, 30, 30, cv2.BORDER_CONSTANT, value=[255,255,255])
-  img_with_padding_resized = cv2.resize(img_with_padding, (28,28))
+  cv2.imwrite('./dev_images/'+str(number)+'/'+ str(np.random.randint(0, high=100000000000000000)) +'.png', small)
 
-  rows, cols = img_with_padding.shape
-  
-  # Translate images
-  M1 = np.float32([[1,0,30],[0,1,0]])
-  M2 = np.float32([[1,0,0],[0,1,30]])
-  M3 = np.float32([[1,0,-30],[0,1,0]])
-  M4 = np.float32([[1,0,0],[0,1,-30]])
-
-  M1_img = cv2.warpAffine(img_with_padding,M1,(cols,rows), borderMode=cv2.BORDER_CONSTANT, borderValue=[255,255,255])
-  M2_img = cv2.warpAffine(img_with_padding,M2,(cols,rows), borderMode=cv2.BORDER_CONSTANT, borderValue=[255,255,255])
-  M3_img = cv2.warpAffine(img_with_padding,M3,(cols,rows), borderMode=cv2.BORDER_CONSTANT, borderValue=[255,255,255])
-  M4_img = cv2.warpAffine(img_with_padding,M4,(cols,rows), borderMode=cv2.BORDER_CONSTANT, borderValue=[255,255,255])
-
-  M1_img_resized = cv2.resize(M1_img, (28,28))
-  M2_img_resized = cv2.resize(M2_img, (28,28))
-  M3_img_resized = cv2.resize(M3_img, (28,28))
-  M4_img_resized = cv2.resize(M4_img, (28,28))
-  
-  # Rotate image matrix 
-  R_1 = cv2.getRotationMatrix2D((cols/2,rows/2),15,1)
-  R_2 = cv2.getRotationMatrix2D((cols/2,rows/2),-15,1)
-
-
-  M1_R_1_img = cv2.warpAffine(M1_img,R_1,(cols,rows), borderMode=cv2.BORDER_CONSTANT, borderValue=[255,255,255])
-  M1_R_2_img = cv2.warpAffine(M1_img,R_2,(cols,rows), borderMode=cv2.BORDER_CONSTANT, borderValue=[255,255,255])
-
-  M2_R_1_img = cv2.warpAffine(M2_img,R_1,(cols,rows), borderMode=cv2.BORDER_CONSTANT, borderValue=[255,255,255])
-  M2_R_2_img = cv2.warpAffine(M2_img,R_2,(cols,rows), borderMode=cv2.BORDER_CONSTANT, borderValue=[255,255,255])
-
-  M3_R_1_img = cv2.warpAffine(M3_img,R_1,(cols,rows), borderMode=cv2.BORDER_CONSTANT, borderValue=[255,255,255])
-  M3_R_2_img = cv2.warpAffine(M3_img,R_2,(cols,rows), borderMode=cv2.BORDER_CONSTANT, borderValue=[255,255,255])
-
-  M4_R_1_img = cv2.warpAffine(M4_img,R_1,(cols,rows), borderMode=cv2.BORDER_CONSTANT, borderValue=[255,255,255])
-  M4_R_2_img = cv2.warpAffine(M4_img,R_2,(cols,rows), borderMode=cv2.BORDER_CONSTANT, borderValue=[255,255,255])
-
-  M1_R_1_img_resized = cv2.resize(M1_R_1_img, (28,28))
-  M1_R_2_img_resized  = cv2.resize(M1_R_2_img, (28,28))
-  M2_R_1_img_resized  = cv2.resize(M2_R_1_img, (28,28))
-  M2_R_2_img_resized  = cv2.resize(M2_R_2_img, (28,28))
-  M3_R_1_img_resized = cv2.resize(M3_R_1_img, (28,28))
-  M3_R_2_img_resized = cv2.resize(M3_R_2_img, (28,28))
-  M4_R_1_img_resized = cv2.resize(M4_R_1_img, (28,28))
-  M4_R_2_img_resized = cv2.resize(M4_R_2_img, (28,28))
-
-  cv2.imwrite('img_with_padding_resized.png', img_with_padding_resized)
-  cv2.imwrite('M1_img.png', M1_img_resized)
-  cv2.imwrite('M2_img.png', M2_img_resized)
-  cv2.imwrite('M3_img.png', M3_img_resized)
-  cv2.imwrite('M4_img.png', M4_img_resized)
-  cv2.imwrite('M1_R_1_img.png', M1_R_1_img_resized)
-  cv2.imwrite('M1_R_2_img.png', M1_R_2_img_resized)
-  cv2.imwrite('M2_R_1_img.png', M2_R_1_img_resized)
-  cv2.imwrite('M2_R_2_img.png', M2_R_2_img_resized)
-  cv2.imwrite('M3_R_1_img.png', M3_R_1_img_resized)
-  cv2.imwrite('M3_R_2_img.png', M3_R_2_img_resized)
-  cv2.imwrite('M4_R_1_img.png', M4_R_1_img_resized)
-  cv2.imwrite('M4_R_2_img.png', M4_R_2_img_resized)
-
-  cv2.imwrite('./training_images/'+str(number)+'/'+ str(np.random.randint(0, high=100000000000000000)) +'.png', small)
-  cv2.imwrite('./training_images/'+str(number)+'/'+ str(np.random.randint(0, high=100000000000000000)) +'.png', img_with_padding_resized)
-  cv2.imwrite('./training_images/'+str(number)+'/'+ str(np.random.randint(0, high=100000000000000000)) +'.png', M1_img_resized)
-  cv2.imwrite('./training_images/'+str(number)+'/'+ str(np.random.randint(0, high=100000000000000000)) +'.png', M2_img_resized)
-  cv2.imwrite('./training_images/'+str(number)+'/'+ str(np.random.randint(0, high=100000000000000000)) +'.png', M3_img_resized)
-  cv2.imwrite('./training_images/'+str(number)+'/'+ str(np.random.randint(0, high=100000000000000000)) +'.png', M4_img_resized)
-  cv2.imwrite('./training_images/'+str(number)+'/'+ str(np.random.randint(0, high=100000000000000000)) +'.png', M1_R_1_img_resized)
-  cv2.imwrite('./training_images/'+str(number)+'/'+ str(np.random.randint(0, high=100000000000000000)) +'.png', M1_R_2_img_resized)
-  cv2.imwrite('./training_images/'+str(number)+'/'+ str(np.random.randint(0, high=100000000000000000)) +'.png', M2_R_1_img_resized)
-  cv2.imwrite('./training_images/'+str(number)+'/'+ str(np.random.randint(0, high=100000000000000000)) +'.png', M2_R_2_img_resized)
-  cv2.imwrite('./training_images/'+str(number)+'/'+ str(np.random.randint(0, high=100000000000000000)) +'.png', M3_R_1_img_resized)
-  cv2.imwrite('./training_images/'+str(number)+'/'+ str(np.random.randint(0, high=100000000000000000)) +'.png', M3_R_2_img_resized)
-  cv2.imwrite('./training_images/'+str(number)+'/'+ str(np.random.randint(0, high=100000000000000000)) +'.png', M4_R_1_img_resized)
-  cv2.imwrite('./training_images/'+str(number)+'/'+ str(np.random.randint(0, high=100000000000000000)) +'.png', M4_R_2_img_resized)
-  
   return jsonify({}), 200
 
+  
 if __name__ == '__main__':
-  app.run(port='5002')
+  data = cv2.imread('small.png', cv2.IMREAD_GRAYSCALE)
+  data = ValueInvert(data)
+
+  src = 'dev_images'
+  dest = 'dev_images_centered'
+
+  for number in range(0, 9):
+    for filename in os.listdir(src + '/' +str(number)):
+      img = cv2.imread(os.path.join(src, str(number), filename), cv2.IMREAD_GRAYSCALE)
+      if img is not None:
+        # img = ValueInvert(img)
+        # X.append(img.reshape(image_size * image_size))
+        # Y.append(number)
+        cv2.imwrite(os.path.join(dest, str(number), filename), center_image(img))
+
+  # img = cv2.imread(os.path.join(src, '5', '96499687945525348.png'), cv2.IMREAD_GRAYSCALE)
+  # plt.imshow(img)
+  # plt.show()
+
+  # test = center_image(img)
+  # plt.imshow(test)
+  # plt.show()
+
+
+  # app.run(port='5002')
+
--- mnist.py
+++ mnist.py
@@ -4,71 +4,74 @@ import cv2
 import os
 from utils import ValueInvert
 
+# MNIST_train_num = 60000
+# MNIST_test_num = 10000
 image_size = 28
 
-# def get_X_train(num_train):
-#   f_train = gzip.open('train-images-idx3-ubyte.gz','r')
-#   f_train.read(16)
-#   buf = f_train.read(image_size * image_size * num_train)
-#   data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
-#   data = data.reshape(num_train, image_size * image_size)
+def get_MNIST_X_train(num_train):
+  f_train = gzip.open('train-images-idx3-ubyte.gz','r')
+  f_train.read(16)
+  buf = f_train.read(image_size * image_size * num_train)
+  data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
+  data = data.reshape(num_train, image_size * image_size)
 
-#   return data
+  return data
 
-# def get_Y_train(num_train):
-#   f_labels = gzip.open('train-labels-idx1-ubyte.gz','r')
-#   f_labels.read(8)
-#   buf = f_labels.read(num_train)
-#   labels = np.frombuffer(buf, dtype=np.uint8).astype(np.int64)
+def get_MNIST_Y_train(num_train):
+  f_labels = gzip.open('train-labels-idx1-ubyte.gz','r')
+  f_labels.read(8)
+  buf = f_labels.read(num_train)
+  labels = np.frombuffer(buf, dtype=np.uint8).astype(np.int64)
 
-#   return labels
-
-
-# def get_X_test(num_test):
-#   f_train = gzip.open('t10k-images-idx3-ubyte.gz','r')
-#   f_train.read(16)
-#   buf = f_train.read(image_size * image_size * num_test)
-#   data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
-#   data = data.reshape(num_test, image_size * image_size)
-
-#   return data
-
-# def get_Y_test(num_test):
-#   f_labels = gzip.open('t10k-labels-idx1-ubyte.gz','r')
-#   f_labels.read(8)
-#   buf = f_labels.read(num_test)
-#   labels = np.frombuffer(buf, dtype=np.uint8).astype(np.int64)
-
-#   return labels
-
-# def get_train(num):
-#   X_train = get_X_train(num)
-#   Y_train = get_Y_train(num)
-
-#   return X_train, Y_train
+  return labels
 
 
-# def get_test(num):
-#   X_test = get_X_test(num)
-#   Y_test = get_Y_test(num)
+def get_MNIST_X_test(num_test):
+  f_train = gzip.open('t10k-images-idx3-ubyte.gz','r')
+  f_train.read(16)
+  buf = f_train.read(image_size * image_size * num_test)
+  data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
+  data = data.reshape(num_test, image_size * image_size)
 
-#   return X_test, Y_test
+  return data
 
-def get_dev_test():
+def get_MNIST_Y_test(num_test):
+  f_labels = gzip.open('t10k-labels-idx1-ubyte.gz','r')
+  f_labels.read(8)
+  buf = f_labels.read(num_test)
+  labels = np.frombuffer(buf, dtype=np.uint8).astype(np.int64)
+
+  return labels
+
+def get_MNIST_train(num):
+  X_train = get_MNIST_X_train(num)
+  Y_train = get_MNIST_Y_train(num)
+
+  return X_train, Y_train
+
+
+def get_MNIST_test(num):
+  X_test = get_MNIST_X_test(num)
+  Y_test = get_MNIST_Y_test(num)
+
+  return X_test, Y_test
+
+def get_CANVAS_dev():
   folder = 'dev_images'
   X = []
   Y = []
   for number in range(0, 9):
     for filename in os.listdir(folder + '/' +str(number)):
       img = cv2.imread(os.path.join(folder, str(number), filename), cv2.IMREAD_GRAYSCALE)
-      img = ValueInvert(img)
-      X.append(img.reshape(image_size * image_size))
-      Y.append(number)
+      if img is not None:
+        img = ValueInvert(img)
+        X.append(img.reshape(image_size * image_size))
+        Y.append(number)
   
   return np.array(X), np.array(Y)
 
-def get_train():
-  folder = 'training_images'
+def get_CANVAS_test():
+  folder = 'test_images'
   X = []
   Y = []
   for number in range(0, 9):
